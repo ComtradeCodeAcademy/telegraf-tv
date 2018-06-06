8a132d789e3a763f177745865ee2d8c6c4b5769e
Predrag Jevtic <designbyheart@gmail.com>
2018-06-06 20:27:53 +0200

    added home controller with cells

---
+++ TelegrafTV/TelegrafTV/Cells/cellItems/FeaturedVideoCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeaturedVideoCollectionViewCell.swift
+//  TelegrafTV
+//
+//  Created by Pedja Jevtic on 6/6/18.
+//  Copyright © 2018 Pedja Jevtic. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedVideoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+}
