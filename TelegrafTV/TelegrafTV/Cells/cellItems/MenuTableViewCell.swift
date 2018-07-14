b75178b08640a486c73cd1c9eb26acc78c023195
O <o@Darko-Baturs-iMac.local>
2018-07-14 21:10:53 +0200

    TableView menuCell

---
+++ TelegrafTV/TelegrafTV/Cells/cellItems/MenuTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MenuTableViewCell.swift
+//  TelegrafTV
+//
+//  Created by O on 7/14/18.
+//  Copyright © 2018 Pedja Jevtic. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
