b06db39e5d7172ab37f4a8018c35019260476c16
O <o@Darko-Baturs-iMac.local>
2018-07-14 20:46:55 +0200

    Init CategoryItemsView

--- TelegrafTV/TelegrafTV/Scenes/CategoryItems/CategoryItemsView.swift
+++ TelegrafTV/TelegrafTV/Scenes/CategoryItems/CategoryItemsView.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class CategoryItemsView: UIView {
 
-   
-
+    @IBOutlet var categoryItemsView: UIView!
+    
+    override init(frame:CGRect) {
+        super.init(frame: frame)
+        commonInit()
+        
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    
+   func commonInit() {
+    Bundle.main.loadNibNamed("CategoryItemsView", owner: self, options: nil)
+    addSubview(categoryItemsView)
+    categoryItemsView.frame = self.bounds
+    categoryItemsView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    
+    }
+    
+    
 }
