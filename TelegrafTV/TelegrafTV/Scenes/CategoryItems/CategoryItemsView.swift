0d5f9a87fd40ead2f7be4f7bbf4002de7e8bad50
O <o@Darko-Baturs-iMac.local>
2018-07-19 22:04:23 +0200

    add AwakeFromNib

--- TelegrafTV/TelegrafTV/Scenes/CategoryItems/CategoryItemsView.swift
+++ TelegrafTV/TelegrafTV/Scenes/CategoryItems/CategoryItemsView.swift
@@ -8,12 +8,18 @@
 
 import UIKit
 
-class CategoryItemsView: UIView {
+class CategoryItemsView: UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout  {
 
     @IBOutlet var categoryItemsView: UIView!
     @IBOutlet weak var collectionView: UICollectionView!
     
      let MyCollectionViewCellId: String = "MyCollectionViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let nibCell = UINib(nibName: MyCollectionViewCellId, bundle: nil)
+        collectionView.register(nibCell, forCellWithReuseIdentifier: MyCollectionViewCellId)
+    }
     override init(frame:CGRect) {
         super.init(frame: frame)
         commonInit()
@@ -33,15 +39,49 @@ class CategoryItemsView: UIView {
     
     }
     
-    func updateUI() {
+    
+//    func numberOfSections(in collectionView: UICollectionView) -> Int {
+//        return 1
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 30
+//    }
+//
+//
+//    func collectionView(_ collectionView: UICollectionView,
+//                        layout collectionViewLayout: UICollectionViewLayout,
+//                        insetForSectionAt section: Int) -> UIEdgeInsets {
+//        let inset = 100
+//        return UIEdgeInsetsMake(CGFloat(inset), CGFloat(inset), CGFloat(inset), CGFloat(inset))
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView,
+//                        layout collectionViewLayout: UICollectionViewLayout,
+//                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize.init(width: 240 , height: 240)
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCellId, for: indexPath) as! MyCollectionViewCell
+//
+//        cell.dateLbl.text = "28.februar.2018"
+//        cell.timeLbl.text = "5:40"
+//        cell.titleLbl.text = "Grupa IOS programera iz Comtrade postigla uspeh"
+//
+//        return cell
+//    }
+//
+//    func updateUI() {
+//
+//        self.categoryItemsView.backgroundColor = .blue
+//        self.collectionView.backgroundColor = .yellow
+//
+        //MARK: Registar UI CV item cell
         
-        self.categoryItemsView.backgroundColor = .blue
-        self.collectionView.backgroundColor = .yellow
+      
         
-        //MARK: Registar UI CV item cell 
         
-        let nibCell = UINib(nibName: MyCollectionViewCellId, bundle: nil)
-        collectionView.register(nibCell, forCellWithReuseIdentifier: MyCollectionViewCellId)
     }
     
 }
