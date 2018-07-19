2667821c201d8af0003a5457d742c469d0c5fbbc
O <o@Darko-Baturs-iMac.local>
2018-07-19 20:04:51 +0200

    add ConstrainsC

---
+++ TelegrafTV/TelegrafTV/Cells/cellItems/MyCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MyCollectionViewCell.swift
+//  TelegrafTV
+//
+//  Created by O on 7/19/18.
+//  Copyright © 2018 Pedja Jevtic. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+  
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
