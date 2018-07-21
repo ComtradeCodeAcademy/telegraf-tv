3eab7db4a5b212208489d8f0dd1679792b6da4ca
O <o@Darko-Baturs-iMac.local>
2018-07-21 06:56:24 +0200

    Add App Security Settings

--- TelegrafTV/TelegrafTV/Scenes/CategoryItems/CategoryItemsView.swift
+++ TelegrafTV/TelegrafTV/Scenes/CategoryItems/CategoryItemsView.swift
@@ -14,7 +14,9 @@ class CategoryItemsView: UIView, UICollectionViewDelegate, UICollectionViewDataS
     @IBOutlet weak var collectionView: UICollectionView!
     
      let MyCollectionViewCellId: String = "MyCollectionViewCell"
-    let imageArray: [String] = ["img1", "img2", "img3"]
+//    let baseURL = "http://tv.tf.rs/"
+//    var categorys = [[String: AnyObject]]()
+    
     //MARK: Registar UI CV item cell
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -71,8 +73,9 @@ class CategoryItemsView: UIView, UICollectionViewDelegate, UICollectionViewDataS
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCellId, for: indexPath) as! MyCollectionViewCell
+//        let category = self.categorys[indexPath.row]
         
-        cell.dateLbl.text = "28.februar.2018"
+        cell.dateLbl.text = "28.Februar.2018"
         cell.timeLbl.text = "5:40"
         cell.titleLbl.text = "NOLE DRHTAVIM GLASOM pred srpskim novinarima rekao ono ČEGA SE SVI PLAŠE: Ne mogu više ovako, od danas do sutra!"
         
@@ -85,12 +88,34 @@ class CategoryItemsView: UIView, UICollectionViewDelegate, UICollectionViewDataS
 
         self.categoryItemsView.backgroundColor = .gray
         self.collectionView.backgroundColor = .clear
-
-     
-        
-      
-        
         
     }
+//    func loadApi() {
+//
+//        guard let url = URL.init(string: baseURL) else { return }
+//
+//        let urlRequest = URLRequest.init(url: url)
+//
+//        let task = URLSession.shared.dataTask(with: urlRequest) {(data, response, error) in
+//            if let usableData = data {
+//                do {
+//
+//
+//                    if let jsonData = try JSONSerialization.jsonObject(with: usableData, options: JSONSerialization.ReadingOptions.allowFragments) as? [[String: AnyObject]] {
+//
+//                        DispatchQueue.main.async {
+//                            self.categorys = jsonData
+//                            self.collectionView.reloadData()
+//
+//                        }
+//                    }
+//                } catch let myJSONError {
+//                    print(myJSONError)
+//                }
+//            }
+//
+//        }
+//        task.resume()
+//    }
     
 }
