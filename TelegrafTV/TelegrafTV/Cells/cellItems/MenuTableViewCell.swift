5f51dd9b5895a63c1cc20bf6c38ead90abeb041d
Borislav Lepsanovic <borislavlepsanovic@gmail.com>
2018-07-22 17:44:18 +0200

    Add Controller, Cell and XIB

--- TelegrafTV/TelegrafTV/Cells/cellItems/MenuTableViewCell.swift
+++ TelegrafTV/TelegrafTV/Cells/cellItems/MenuTableViewCell.swift
@@ -2,7 +2,7 @@
 //  MenuTableViewCell.swift
 //  TelegrafTV
 //
-//  Created by O on 7/14/18.
+//  Created by macbook air on 7/22/18.
 //  Copyright © 2018 Pedja Jevtic. All rights reserved.
 //
 
@@ -10,14 +10,9 @@ import UIKit
 
 class MenuTableViewCell: UITableViewCell {
 
-//    @IBOutlet weak var selectionImgView: UIImageView!
-    @IBOutlet weak var titleLbl: UILabel!
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        
-        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
