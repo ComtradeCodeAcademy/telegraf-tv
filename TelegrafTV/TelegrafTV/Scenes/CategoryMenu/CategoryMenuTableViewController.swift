63d9fd9bc19dec3d02fedd9b4bb6dcbf0a35f782
O <o@Darko-Baturs-iMac.local>
2018-07-26 00:29:01 +0200

    add CGRectMake func

--- TelegrafTV/TelegrafTV/Scenes/CategoryMenu/CategoryMenuTableViewController.swift
+++ TelegrafTV/TelegrafTV/Scenes/CategoryMenu/CategoryMenuTableViewController.swift
@@ -83,8 +83,10 @@ class CategoryMenuTableViewController: UITableViewController {
         }
     }
     
-   
-    
+    func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
+        
+        return CGRect(x: x, y: y, width: width, height: height)
+    }
 }
 
 
