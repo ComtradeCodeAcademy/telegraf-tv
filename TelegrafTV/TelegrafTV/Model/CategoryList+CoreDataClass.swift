0416d41ad099b230053da2ac6b09a5e42c553b51
O <o@Darko-Baturs-iMac.local>
2018-08-02 22:59:06 +0200

    crate new group Model with NS subclass

---
+++ TelegrafTV/TelegrafTV/Model/CategoryList+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  CategoryList+CoreDataClass.swift
+//  TelegrafTV
+//
+//  Created by O on 8/2/18.
+//  Copyright © 2018 Pedja Jevtic. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class CategoryList: NSManagedObject {
+
+}
