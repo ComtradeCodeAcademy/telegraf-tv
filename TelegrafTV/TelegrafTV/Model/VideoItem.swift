6209bf2f3c5347a21875a151de910ffb82e41328
O <o@Darko-Baturs-iMac.local>
2018-08-05 21:16:25 +0200

    create VideoItem struct

---
+++ TelegrafTV/TelegrafTV/Model/VideoItem.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  TelegrafTV
+//
+//  Created by O on 8/5/18.
+//  Copyright © 2018 Pedja Jevtic. All rights reserved.
+//
+
+import Foundation
+
+struct VideoItem {
+    
+    let id: Int,
+    title: String,
+    duration: String,
+    imageURL: String,
+    data: String,
+    videoURL: String
+    
+}
