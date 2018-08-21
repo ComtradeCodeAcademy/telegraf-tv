6d548491f6e5760b5401fd967a64bdae1f41716e
Nebojsa Krstovic <krstovicnebojsa@gmail.com>
2018-08-21 22:48:55 +0200

    working player

--- TelegrafTV/TelegrafTV/Scenes/VPlayer and Category details/VPlayerViewController.swift
+++ TelegrafTV/TelegrafTV/Scenes/VPlayer and Category details/VPlayerViewController.swift
@@ -16,13 +16,28 @@ class VPlayerViewController: UIViewController, AVPlayerViewControllerDelegate {
     @IBOutlet var videoCategoryDetailsView: VideoCategoryDetailsView!
     
     let controller = AVPlayerViewController()
-    
+    var player: AVPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
       
-        
+        videoCategoryDetailsView.isHidden = true
         videoCategoryDetailsView.updateUI()
+        player = AVPlayer()
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        playVideo()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+//        NotificationCenter.default.removeObserver(self)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,32 +45,29 @@ class VPlayerViewController: UIViewController, AVPlayerViewControllerDelegate {
         // Dispose of any resources that can be recreated.
     }
    
-    @IBAction func playVideo(_ sender: UIButton) {
+    @IBAction func playVideo() {
         
         
         guard let url = URL(string: "https://cdn.telegraf.tv/2018/07/21/316162f6c4/2107bezeodhrane.m3u8") else {
             return
     }
         
-        let player = AVPlayer(url: url)
+        self.player = AVPlayer(url: url)
         controller.player = player
         
         
-        NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: controller.player?.currentItem)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(playerDidFinishPlaying),
+                                               name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+                                               object: controller.player?.currentItem)
+        
             present(controller, animated: true) {
-            player.play()
-                
-                if self.controller.player?.timeControlStatus == AVPlayerTimeControlStatus.paused {
+                self.player?.play()
+        }
                     self.controller.view.addSubview(self.videoCategoryDetailsView)
                     self.videoCategoryDetailsView.isUserInteractionEnabled = true
-                    
-                } else {
-                    print("Nema")
-                }
-            
-            
-        }
-        
+    
     }
     @objc func playerDidFinishPlaying(note: NSNotification) {
         self.controller.dismiss(animated: true)
