574e1268a682e71641087f2f8628a213ce6b7ac1
Darko Batur <darkobatur@gmail.com>
2018-09-07 19:49:53 +0200

    populate live cell

--- TelegrafTV/TelegrafTV/Cells/cellItems/liveCollectionViewCell.swift
+++ TelegrafTV/TelegrafTV/Cells/cellItems/liveCollectionViewCell.swift
@@ -19,15 +19,16 @@ class liveCollectionViewCell: UICollectionViewCell {
      liveImage.adjustsImageWhenAncestorFocused = true
     }
     
-//    func configureCell(videos: VideoItem) {
-//        DispatchQueue.main.async {
-//            self.liveImage.loadImageUsingCacheWithURLString(videos.imageURL!, placeHolder: UIImage(named: "placeholder"))
-//            self.liveTitle.text = videos.title
-//            self.liveTitle.numberOfLines = 1
-//        }
-      func configureCell() {
-        self.liveImage.image = UIImage(named: "img1")
-        self.liveTitle.text = "PRENOS UTAKMICE"
-        self.liveTitle.numberOfLines = 1
+    func configureCell(videos: VideoItem) {
+        DispatchQueue.main.async {
+            self.liveImage.loadImageUsingCacheWithURLString(videos.imageURL!, placeHolder: UIImage(named: "placeholder"))
+            self.liveTitle.text = videos.title
+        }
 }
 }
+//      func configureCell() {
+//        self.liveImage.image = UIImage(named: "img1")
+//        self.liveTitle.text = "PRENOS UTAKMICE"
+//        self.liveTitle.numberOfLines = 1
+//}
+
